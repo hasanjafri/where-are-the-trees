f81544974fd3f2806ec0d3a0d2daf30fd1b164db
Hasan Jafri <hasan.jafri7@gmail.com>
2018-11-21 18:47:14 -0500

    Add water reflection shader, add swim animation, work on Level 2

---
+++ Assets/Standard Assets/Water2D Surface/scripts/Water2DScript.cs
@@ -0,0 +1,27 @@
+﻿// 2016 - Damien Mayance (@Valryon)
+// Source: https://github.com/valryon/water2d-unity/
+using UnityEngine;
+
+/// <summary>
+/// Water surface script (update the shader properties).
+/// </summary>
+public class Water2DScript : MonoBehaviour
+{
+  public Vector2 speed = new Vector2(0.01f, 0f);
+
+  private Renderer rend;
+  private Material mat;
+
+  void Awake()
+  {
+    rend = GetComponent<Renderer>();
+    mat = rend.material;
+  }
+
+  void LateUpdate()
+  {
+    Vector2 scroll = Time.deltaTime * speed;
+
+    mat.mainTextureOffset += scroll;
+  }
+}
---
+++ Assets/Standard Assets/Water2D Surface/scripts/WaterReflectableScript.cs
@@ -0,0 +1,70 @@
+﻿// 2016 - Damien Mayance (@Valryon)
+// Source: https://github.com/valryon/water2d-unity/
+using UnityEngine;
+
+/// <summary>
+/// Automagically create a water reflect for a sprite.
+/// </summary>
+[RequireComponent(typeof(SpriteRenderer))]
+public class WaterReflectableScript : MonoBehaviour
+{
+  #region Members
+
+  [Header("Reflect properties")]
+  public Vector3 localPosition = new Vector3(0, -0.25f, 0);
+  public Vector3 localRotation = new Vector3(0, 0, -180);
+  [Tooltip("Optionnal: force the reflected sprite. If null it will be a copy of the source.")]
+  public Sprite sprite;
+  public string spriteLayer = "Default";
+  public int spriteLayerOrder = -5;
+
+  private SpriteRenderer spriteSource;
+  private SpriteRenderer spriteRenderer;
+
+  #endregion
+
+  #region Timeline
+
+  void Awake()
+  {
+    GameObject reflectGo = new GameObject("Water Reflect");
+    reflectGo.transform.parent = this.transform;
+    reflectGo.transform.localPosition = localPosition;
+    reflectGo.transform.localRotation = Quaternion.Euler(localRotation);
+    reflectGo.transform.localScale = new Vector3(reflectGo.transform.localScale.x, reflectGo.transform.localScale.y, reflectGo.transform.localScale.z);
+
+    spriteRenderer = reflectGo.AddComponent<SpriteRenderer>();
+    spriteRenderer.sortingLayerName = spriteLayer;
+    spriteRenderer.sortingOrder = spriteLayerOrder;
+    
+    spriteSource = GetComponent<SpriteRenderer>();
+  }
+
+  void OnDestroy()
+  {
+    if (spriteRenderer != null)
+    {
+      Destroy(spriteRenderer.gameObject);
+    }
+  }
+
+  void LateUpdate()
+  {
+    if (spriteSource != null)
+    {
+      if (sprite == null)
+      {
+        spriteRenderer.sprite = spriteSource.sprite;
+      }
+      else
+      {
+        spriteRenderer.sprite = sprite;
+      }
+      spriteRenderer.flipX = spriteSource.flipX;
+      spriteRenderer.flipY = spriteSource.flipY;
+      spriteRenderer.color = spriteSource.color;
+    }
+  }
+
+  #endregion
+}
